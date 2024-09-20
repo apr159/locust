ef7386bd1adf4c71b2b126c657bc27c8b8a91e4b
apr_159 <apr_159@yahoo.com>
2024-09-19 22:18:31 -0600

    add

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,10 @@
 
 
 from app.execute._app import Application
+from app.execute._create_dataset import CreateDataset
+
+
+from app.ui._app import MainApp
 
 input_variables = [
     {
@@ -25,5 +29,18 @@ input_variables = [
     
 
 
-app = Application(input_variables)
-app.execute({'Latitud':20.57,'Longitud':-90.22524},'2022-10-10')
+#app = Application(input_variables)
+#app.execute({'Latitud':20.57,'Longitud':-90.22524},'2022-10-10')
+#cd = CreateDataset()
+#cd.execute('2022',96,'gee-coordinates-2022/', variables)
+#cd.create_ts_file('2023')
+#cd.catch22_transform('2022')
+
+
+  
+ 
+  
+  
+# Driver Code
+app = MainApp()
+app.mainloop()
